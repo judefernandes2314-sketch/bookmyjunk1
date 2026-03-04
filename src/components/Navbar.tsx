e03521d07ca19f300de7cc472276752371892f31
judefernandes2314-sketch <judefernandes.2314@gmail.com>
2026-03-04 15:00:51 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-xl border-b border-border/50">
       <div className="container mx-auto flex items-center justify-between h-16 px-4">
         <a href="/" className="flex items-center gap-2.5">
-          <img src={logoTBG} alt="BookMyJunk" className="h-18 w-auto" />
+          <img src={logoTBG} alt="BookMyJunk" className="h-16 w-auto" />
         </a>
         <div className="hidden md:flex items-center gap-7">
           {links.map((l) =>
